92f9f4e67490201f9f34c5dd4131c33492dbc836
nielle003 <138173463+nielle003@users.noreply.github.com>
2025-12-07 11:24:12 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'patient_record_system',
+  webDir: 'www'
+};
+
+export default config;
