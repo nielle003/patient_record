3a8539dc6066e2667c8933a0570bfed1ad5e0787
nielle003 <138173463+nielle003@users.noreply.github.com>
2025-12-21 12:42:46 +0800

    new title

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'I Love my Dentist',
+  appName: 'I❤️myDentist',
   webDir: 'www'
 };
 
